59dc0911d281d052162be0054110e7e01b28c23b
Ulaganathan <ulaganathanselvam2000@gmail.com>
2023-02-28 19:52:25 +0530

    Implemented timer feature

--- index.js
+++ index.js
@@ -14,9 +14,25 @@ let hourHand = 0, minuteHand = 0, secondHand = 0, milliSecondHand = 0;
 let startTimer;
 start.addEventListener("click", () => {
     startTimer = setInterval(() => {
-        milliSecondHand++;
-        milliSeconds.innerText = milliSecondHand;
-    }, 1)
+        milliSecondHand += 10;
+        if(milliSecondHand == 1000){
+            milliSecondHand = 0;
+            secondHand++;
+            if(secondHand == 60){
+                secondHand = 0;
+                minuteHand++;
+                if(minuteHand == 60){
+                    minuteHand = 0;
+                    hourHand++;
+                    if(hourHand == 24) hourHand = 0;
+                }
+            }
+        }
+        hours.innerText = hourHand < 10 ? "0"+hourHand : hourHand;
+        minutes.innerText = minuteHand < 10 ? "0"+minuteHand : minuteHand;
+        seconds.innerText = secondHand < 10 ? "0"+secondHand : secondHand;
+        milliSeconds.innerText = milliSecondHand < 10 ? "00"+milliSecondHand : milliSecondHand < 100 ? "0"+milliSecondHand : milliSecondHand;
+    }, 10)
 })
 
 pause.addEventListener("click", () => {
@@ -24,7 +40,11 @@ pause.addEventListener("click", () => {
 })
 
 reset.addEventListener("click", () => {
-
+    clearInterval(startTimer);
+    hours.innerText = "00";
+    minutes.innerText = "00";
+    seconds.innerText = "00";
+    milliSeconds.innerText = "000";
 })
 
 cast.addEventListener("click", () => {
